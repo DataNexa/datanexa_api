3cb9825a529c0536507c773e913c0e8d388c8d61
Andrei Coelho <andreifcoelho@gmail.com>
2024-06-18 01:06:28 -0300

    serviços de campanha

---
+++ src/repositories/campanhas.repo.ts
@@ -0,0 +1,145 @@
+import { execute, query, multiTransaction } from "../util/query"
+
+interface campanhas_i {
+    client_id:number,
+    nome:string,
+    descricao:string,
+    ativo:number
+}
+
+interface create_response {
+    error:boolean,
+    message:string,
+    insertId:number,
+    code:number
+}
+
+interface unique_response {
+    error:boolean,
+    message:string,
+    code:number,
+    row?:campanhas_i
+}
+
+const campanhas_repo = {
+        
+    
+    list: async (client_id:number, injectString:string=''):Promise<campanhas_i[]|false> => {
+            
+        const resp = await query(` 
+        SELECT  campanhas.id,  campanhas.client_id,  campanhas.nome,  campanhas.descricao,  campanhas.ativo
+        from campanhas 
+             join client on campanhas.client_id = client.id 
+ 
+         WHERE  client.id = ? 
+        ${injectString}`, {
+            binds:[client_id]
+        })
+
+        if(resp.error) return false 
+
+        return (resp.rows as campanhas_i[])
+    },    
+    
+    unique: async (client_id:number,id:number):Promise<unique_response> =>  {
+        
+        const resp = await query(` 
+        SELECT  campanhas.id,  campanhas.client_id,  campanhas.nome,  campanhas.descricao,  campanhas.ativo
+        from campanhas 
+             join client on campanhas.client_id = client.id 
+ 
+         WHERE  client.id = ? 
+            and campanhas.id = ? `, {
+            binds:[client_id,id]
+        })
+
+        if(resp.error) return {
+            error:true,
+            code:500,
+            message:'Erro no servidor'
+        } 
+        
+        const rows = (resp.rows as campanhas_i[])
+
+        if(rows.length == 0) return {
+            error:true,
+            code:404,
+            message:'Registro não encontrado'
+        }  
+
+        return {
+            error:false,
+            code:200,
+            message:'',
+            row: rows[0]
+        }  
+
+    },    
+    
+    create: async (client_id:number,nome:string,descricao:string,ativo:number):Promise<create_response> => {
+            
+        const resp = await execute(`
+        insert into campanhas(client_id, nome, descricao, ativo) 
+        VALUES (?,?,?,?)
+             join client on campanhas.client_id = client.id 
+
+         WHERE  client.id = ? 
+         `, {
+            binds:[client_id,nome,descricao,ativo,client_id]
+        })
+
+        if(resp.error && resp.error_code == 1062) return {
+            code:500,
+            error:true,
+            message:'Registro Duplicado',
+            insertId:0
+        } 
+
+        if(resp.error && resp.error_code == 1366) return {
+            code:400,
+            error:true,
+            message:'Campo enviado mal formatado',
+            insertId:0
+        } 
+
+        if(resp.error) return {
+            code:500,
+            error:true,
+            message:'Erro no servidor',
+            insertId:0
+        }
+
+        return {
+            code:200,
+            error:false,
+            message:'',
+            insertId: (resp.rows as any).insertId
+        }
+
+    },    
+    
+    update: async (client_id:number,nome:string,descricao:string,ativo:number,id:number):Promise<boolean> => {
+        
+        const resp = await execute(`update campanhas set  client_id = ?,  nome = ?,  descricao = ?,  ativo = ?     join client on campanhas.client_id = client.id 
+         WHERE  client.id = ?  and campanhas.id = ? `, {
+            binds:[client_id,nome,descricao,ativo,client_id,id]
+        })
+
+        return !resp.error
+    },    
+    
+    delete: async (client_id:number,id:number):Promise<boolean> => {
+        
+        const resp = await execute(`
+         delete from campanhas 
+          join client on campanhas.client_id = client.id 
+ 
+        WHERE  client.id = ?   and campanhas.id = ? `, {
+            binds:[client_id,id]
+        })
+
+        return !resp.error
+    }
+}
+
+export { campanhas_repo, campanhas_i }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,10 +3,14 @@ import account_routes from "./account.route"
 import user_routes from "./user.route"
 import globals from "../config/globals"
 import test from "../_testes/test"
+import tarefas_route from "./tarefas.route"
+import campanhas_route from "./campanhas.route"
 export default (app:Express) => {
 
     app.use('/account', account_routes())
     app.use('/user', user_routes())
+    app.use('/tarefas', tarefas_route())
+    app.use('/campanhas', campanhas_route())
     if(!globals.production){
         test()
     }
