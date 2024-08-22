928370344da5b4a1e7aa64f3b5c0451de823ddd7
Andrei Coelho <andreifcoelho@gmail.com>
2024-08-22 01:04:15 -0300

    alterado arquivos de repositorio sql

--- src/repositories/sql/auth_estrutura.sql
+++ src/repositories/sql/auth_estrutura.sql
@@ -12,6 +12,8 @@ create table if not exists account (
     nome varchar(100) not null,
     email varchar(250) not null unique,
     senha varchar(255),
+    confirmed tinyint(1) not null default 0,
+    temporary tinyint(1) not null default 0,
 
     primary key(id)
 )ENGINE=InnoDB;
