1f7542d16ffbfb270b067033fb7b874aa7f10ba8
Andrei Coelho <andreifcoelho@gmail.com>
2025-01-21 16:39:04 -0300

    adicionado o transformador de busca

---
+++ src/libs/SearchTransform.test.ts
@@ -0,0 +1,20 @@
+import SearchTransform from "./SearchTransform"
+
+SearchTransform
+
+test('transformação de texto completo', () => {
+    expect(SearchTransform("+palavra1+palavra2 palavra3 -palavra4 +palavra5")).toMatchObject({
+        obrigatorias: [ 'palavra1 palavra2', 'palavra5' ],
+        podeTer: [ 'palavra3' ],
+        naoPodemTer: [ 'palavra4' ]
+    })
+})
+
+test('transformação de texto apenas com palavras proibidas e compostas', () => {
+    expect(SearchTransform(" -palavra1+palavra2+palavra3 -palavra4+palavra5")).toMatchObject({
+        obrigatorias: [],
+        podeTer: [],
+        naoPodemTer: ['palavra1 palavra2 palavra3', 'palavra4 palavra5']
+    })
+})
+
