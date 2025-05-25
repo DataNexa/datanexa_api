486cbbdb0182b4a6dfdd8ae0491eafee10c97d63
Andrei Coelho <andreifcoelho@gmail.com>
2025-05-24 21:19:33 -0300

    finalizado repositorio de monitoramentos

--- src/repositories/monitoramento.repo.ts
+++ src/repositories/monitoramento.repo.ts
@@ -6,6 +6,7 @@ import { Monitoramento, MonitoramentoFull } from "../types/Monitoramento"
 import updateBuild from "../core/database/updateBuild"
 import Logger from "../util/logger"
 import { ConfigMap, ConfigMapSimple } from "../types/SearchConfig"
+import { OkPacket } from "mysql"
 
 
 const fields:{[key:string]:string} = {
@@ -36,99 +37,6 @@ const defaultMonitoramentoFull:MonitoramentoFull = {
     descricao:'',
     ativo:true,
     cliente_id:0
-}
-
-
-const createQueryMonitoramentos = (ativos:boolean) => {
-
-    let query = `
-        SELECT
-        
-        m.id            AS monitoramento_id,
-        m.client_id,
-        m.titulo,
-        m.descricao,
-        m.create_at,
-        m.data_inicio,
-        m.data_fim,
-        m.ativo,
-
-        
-        gsc.id                 AS google_search_config_id,
-        gsc.dork               AS google_dork,
-        gsc.sites,
-        gsc.noInSites,                   -- coluna renomeada no novo schema
-        gsc.inUrl,
-        gsc.inTitle,
-        gsc.inText,
-        gsc.palavrasExatas      AS google_palavrasExatas,
-        gsc.palavrasQuePodeTer  AS google_palavrasQuePodeTer,
-        gsc.excluirPalavras     AS google_excluirPalavras,
-
-        tsc.id                 AS twitter_search_config_id,
-        tsc.dork               AS twitter_dork,
-        tsc.from_users,
-        tsc.not_from_users,
-        tsc.mentions,
-        tsc.hashtags           AS twitter_hashtags,
-        tsc.palavrasExatas     AS twitter_palavrasExatas,
-        tsc.palavrasQuePodeTer AS twitter_palavrasQuePodeTer,
-        tsc.excluirPalavras    AS twitter_excluirPalavras,
-        tsc.lang               AS twitter_lang,
-
-        ysc.id            AS youtube_search_config_id,
-        ysc.dork          AS youtube_dork,
-        ysc.videoDuration,
-        ysc.videoDefinition,
-        ysc.videoEmbeddable,
-        ysc.ytOrder,
-        ysc.publishAfter,
-        ysc.lang          AS youtube_lang,
-
-        isc.id            AS instagram_search_config_id,
-
-        GROUP_CONCAT(ish.hashtag_value ORDER BY ish.hashtag_value SEPARATOR ',') AS instagram_hashtags,
-        GROUP_CONCAT(ihi.id ORDER BY ish.hashtag_value SEPARATOR ',') AS instagram_hashtag_index_ids
-    `
-
-    query += ativos ? ` FROM (
-                    SELECT m_inner.*
-                    FROM monitoramento m_inner
-                    INNER JOIN (
-                        SELECT client_id, MAX(create_at) AS max_create_at
-                        FROM monitoramento
-                        WHERE ativo = 1
-                        GROUP BY client_id
-                    ) latest ON m_inner.client_id = latest.client_id AND m_inner.create_at = latest.max_create_at
-                    WHERE m_inner.ativo = 1
-                      AND (
-                            (m_inner.data_inicio IS NULL OR m_inner.data_inicio <= NOW())
-                        AND (m_inner.data_fim    IS NULL OR m_inner.data_fim    >= NOW())
-                      )
-                ) m` : `FROM (
-                    SELECT m.*
-                    FROM monitoramento m
-                    JOIN (
-                        SELECT client_id, MIN(id) AS id
-                        FROM monitoramento
-                        WHERE ativo = 0
-                          AND (data_inicio IS NULL OR data_inicio <= NOW())
-                          AND (data_fim    IS NULL OR data_fim    >= NOW())
-                          AND client_id NOT IN (?)
-                        GROUP BY client_id
-                    ) x ON x.id = m.id
-                ) m`
-    query += `  LEFT JOIN google_search_config             gsc ON gsc.monitoramento_id = m.id AND gsc.client_id = m.client_id
-                LEFT JOIN twitter_search_config            tsc ON tsc.monitoramento_id = m.id AND tsc.client_id = m.client_id
-                LEFT JOIN youtube_search_config            ysc ON ysc.monitoramento_id = m.id AND ysc.client_id = m.client_id
-                LEFT JOIN instagram_search_config          isc ON isc.monitoramento_id = m.id AND isc.client_id = m.client_id
-                LEFT JOIN instagram_search_config_hashtags ish ON ish.instagram_search_config_id = isc.id
-                LEFT JOIN index_hashtags_instagram         ihi ON ihi.hashtag_value    = ish.hashtag_value 
-
-                GROUP BY m.id, m.client_id `
-
-    return query
-
 }
 
 
@@ -220,7 +128,126 @@ export default {
             /* -----------------------------------------------------------------
              * MONITORAMENTOS ATIVOS
              * ----------------------------------------------------------------- */
-            const queryMonitoramentos = await conn.query(createQueryMonitoramentos(true))
+            const queryMonitoramentos = await conn.query(`
+                    SELECT
+    
+                        m.id            AS monitoramento_id,
+                        m.client_id,
+                        m.titulo,
+                        m.descricao,
+                        m.create_at,
+                        m.data_inicio,
+                        m.data_fim,
+                        m.ativo,
+
+                        gsc.id                 AS google_search_config_id,
+                        gsc.dork               AS google_dork,
+                        gsc.sites,
+                        gsc.noInSites,                 
+                        gsc.inUrl,
+                        gsc.inTitle,
+                        gsc.inText,
+                        gsc.palavrasExatas      AS google_palavrasExatas,
+                        gsc.palavrasQuePodeTer  AS google_palavrasQuePodeTer,
+                        gsc.excluirPalavras     AS google_excluirPalavras,
+
+                        tsc.id                 AS twitter_search_config_id,
+                        tsc.dork               AS twitter_dork,
+                        tsc.from_users,
+                        tsc.not_from_users,
+                        tsc.mentions,
+                        tsc.hashtags           AS twitter_hashtags,
+                        tsc.palavrasExatas     AS twitter_palavrasExatas,
+                        tsc.palavrasQuePodeTer AS twitter_palavrasQuePodeTer,
+                        tsc.excluirPalavras    AS twitter_excluirPalavras,
+                        tsc.lang               AS twitter_lang,
+
+                        ysc.id            AS youtube_search_config_id,
+                        ysc.dork          AS youtube_dork,
+                        ysc.videoDuration,
+                        ysc.videoDefinition,
+                        ysc.videoEmbeddable,
+                        ysc.ytOrder,
+                        ysc.publishAfter,
+                        ysc.lang          AS youtube_lang,
+
+                        isc.id            AS instagram_search_config_id,
+
+                        GROUP_CONCAT(ish.hashtag_value ORDER BY ish.hashtag_value SEPARATOR ',') AS instagram_hashtags,
+                        GROUP_CONCAT(ihi.id ORDER BY ish.hashtag_value SEPARATOR ',') AS instagram_hashtag_index_ids
+
+                    FROM
+                        monitoramento m
+
+                    LEFT JOIN google_search_config  AS gsc
+                        ON gsc.monitoramento_id = m.id
+                        AND gsc.client_id        = m.client_id
+
+                    LEFT JOIN twitter_search_config AS tsc
+                        ON tsc.monitoramento_id = m.id
+                        AND tsc.client_id        = m.client_id
+
+                    LEFT JOIN youtube_search_config AS ysc
+                        ON ysc.monitoramento_id = m.id
+                        AND ysc.client_id        = m.client_id
+
+                    LEFT JOIN instagram_search_config AS isc
+                        ON isc.monitoramento_id = m.id
+                        AND isc.client_id        = m.client_id
+
+                    LEFT JOIN instagram_search_config_hashtags AS ish
+                        ON ish.instagram_search_config_id = isc.id   
+
+                    LEFT JOIN indexHashtagsInstagram AS ihi
+                        ON ihi.hashtag_instagram_id = ish.id         
+
+                    WHERE
+                        m.ativo = 1
+
+                    GROUP BY
+                        m.id,
+                        m.client_id,
+                        m.titulo,
+                        m.descricao,
+                        m.create_at,
+                        m.data_inicio,
+                        m.data_fim,
+                        m.ativo,
+
+                        gsc.id,
+                        gsc.dork,
+                        gsc.sites,
+                        gsc.noInSites,
+                        gsc.inUrl,
+                        gsc.inTitle,
+                        gsc.inText,
+                        gsc.palavrasExatas,
+                        gsc.palavrasQuePodeTer,
+                        gsc.excluirPalavras,
+
+                        tsc.id,
+                        tsc.dork,
+                        tsc.from_users,
+                        tsc.not_from_users,
+                        tsc.mentions,
+                        tsc.hashtags,
+                        tsc.palavrasExatas,
+                        tsc.palavrasQuePodeTer,
+                        tsc.excluirPalavras,
+                        tsc.lang,
+
+                        ysc.id,
+                        ysc.dork,
+                        ysc.videoDuration,
+                        ysc.videoDefinition,
+                        ysc.videoEmbeddable,
+                        ysc.ytOrder,
+                        ysc.publishAfter,
+                        ysc.lang,
+
+                        isc.id;
+                
+            `)
             if (queryMonitoramentos.error) throw new Error(queryMonitoramentos.error_message)
     
             const monitoramentos = queryMonitoramentos.rows as any[]
@@ -260,6 +287,44 @@ export default {
         }
     },
 
+    desativarMonitoramento: async (client_id:number, monitoramento_id:number):Promise<boolean> => { 
+
+        const res  = await execute(`
+            UPDATE monitoramento m
+            SET ativo = 0
+            WHERE m.id = ?
+            AND m.client_id = ?
+        `, [monitoramento_id, client_id])
+
+       return !res.error
+
+    },
+
+    ativarMonitoramento: async (client_id:number, monitoramento_id:number):Promise<boolean> => {
+
+        const res  = await execute(`
+            UPDATE monitoramento m
+            SET ativo = 1
+            WHERE m.id = ?
+            AND (
+                SELECT COUNT(*)
+                FROM monitoramento
+                WHERE ativo = 1
+                AND client_id = m.client_id
+            ) < (
+                SELECT max_monitoramentos_ativos
+                FROM client_config
+                WHERE client_id = m.client_id
+            )
+            AND m.client_id = ?
+        `, [monitoramento_id, client_id])
+
+        if(res.error) return false
+        const r = res.rows as OkPacket
+        if(r.affectedRows === 0) return false
+        return true
+
+    },
 
     get: async (filter:FilterQuery):Promise<Monitoramento[]|undefined> => {
 
@@ -289,7 +354,7 @@ export default {
         const mon = monitoramento as { [key: string]: any };
 
         const values = Object.keys(monitoramento)
-            .filter(key => key !== "id" && mon[key] !== undefined)
+            .filter(key => key !== "id" && key !== 'ativo' && mon[key] !== undefined)
             .map(key => mon[key]);
 
             
--- src/sql/queryParaPegarOMonitoramento.sql
+++ src/sql/queryParaPegarOMonitoramento.sql
@@ -42,6 +42,125 @@ SELECT
 
     isc.id            AS instagram_search_config_id,
 
+    GROUP_CONCAT(ish.hashtag_value ORDER BY ish.hashtag_value SEPARATOR ',') AS instagram_hashtags,
+    GROUP_CONCAT(ihi.id ORDER BY ish.hashtag_value SEPARATOR ',') AS instagram_hashtag_index_ids
+
+FROM
+    monitoramento m
+
+LEFT JOIN google_search_config  AS gsc
+       ON gsc.monitoramento_id = m.id
+      AND gsc.client_id        = m.client_id
+
+LEFT JOIN twitter_search_config AS tsc
+       ON tsc.monitoramento_id = m.id
+      AND tsc.client_id        = m.client_id
+
+LEFT JOIN youtube_search_config AS ysc
+       ON ysc.monitoramento_id = m.id
+      AND ysc.client_id        = m.client_id
+
+LEFT JOIN instagram_search_config AS isc
+       ON isc.monitoramento_id = m.id
+      AND isc.client_id        = m.client_id
+
+LEFT JOIN instagram_search_config_hashtags AS ish
+       ON ish.instagram_search_config_id = isc.id   
+
+LEFT JOIN indexHashtagsInstagram AS ihi
+       ON ihi.hashtag_instagram_id = ish.id         
+
+WHERE
+    m.ativo = 1
+
+GROUP BY
+    m.id,
+    m.client_id,
+    m.titulo,
+    m.descricao,
+    m.create_at,
+    m.data_inicio,
+    m.data_fim,
+    m.ativo,
+
+    gsc.id,
+    gsc.dork,
+    gsc.sites,
+    gsc.noInSites,
+    gsc.inUrl,
+    gsc.inTitle,
+    gsc.inText,
+    gsc.palavrasExatas,
+    gsc.palavrasQuePodeTer,
+    gsc.excluirPalavras,
+
+    tsc.id,
+    tsc.dork,
+    tsc.from_users,
+    tsc.not_from_users,
+    tsc.mentions,
+    tsc.hashtags,
+    tsc.palavrasExatas,
+    tsc.palavrasQuePodeTer,
+    tsc.excluirPalavras,
+    tsc.lang,
+
+    ysc.id,
+    ysc.dork,
+    ysc.videoDuration,
+    ysc.videoDefinition,
+    ysc.videoEmbeddable,
+    ysc.ytOrder,
+    ysc.publishAfter,
+    ysc.lang,
+
+    isc.id;
+
+
+SELECT
+    
+    m.id            AS monitoramento_id,
+    m.client_id,
+    m.titulo,
+    m.descricao,
+    m.create_at,
+    m.data_inicio,
+    m.data_fim,
+    m.ativo,
+
+    gsc.id                 AS google_search_config_id,
+    gsc.dork               AS google_dork,
+    gsc.sites,
+    gsc.noInSites,                 
+    gsc.inUrl,
+    gsc.inTitle,
+    gsc.inText,
+    gsc.palavrasExatas      AS google_palavrasExatas,
+    gsc.palavrasQuePodeTer  AS google_palavrasQuePodeTer,
+    gsc.excluirPalavras     AS google_excluirPalavras,
+
+    tsc.id                 AS twitter_search_config_id,
+    tsc.dork               AS twitter_dork,
+    tsc.from_users,
+    tsc.not_from_users,
+    tsc.mentions,
+    tsc.hashtags           AS twitter_hashtags,
+    tsc.palavrasExatas     AS twitter_palavrasExatas,
+    tsc.palavrasQuePodeTer AS twitter_palavrasQuePodeTer,
+    tsc.excluirPalavras    AS twitter_excluirPalavras,
+    tsc.lang               AS twitter_lang,
+
+    ysc.id            AS youtube_search_config_id,
+    ysc.dork          AS youtube_dork,
+    ysc.videoDuration,
+    ysc.videoDefinition,
+    ysc.videoEmbeddable,
+    ysc.ytOrder,
+    ysc.publishAfter,
+    ysc.lang          AS youtube_lang,
+
+    isc.id            AS instagram_search_config_id,
+
     GROUP_CONCAT(
         DISTINCT ihi.hashtag_value
         ORDER BY ihi.hashtag_value
@@ -54,9 +173,15 @@ FROM
         FROM monitoramento m_inner
         INNER JOIN (
             SELECT client_id, MAX(create_at) AS max_create_at
-            FROM monitoramento
+            FROM monitoramento as mm
             WHERE ativo = 1
-            GROUP BY client_id
+            GROUP BY mm.client_id 
+            LIMIT (
+                SELECT 
+                    cc.max_monitoramentos_ativos
+                FROM client_config cc
+                WHERE cc.client_id = mm.client_id
+            )
         ) latest 
          ON m_inner.client_id = latest.client_id 
          AND m_inner.create_at = latest.max_create_at
